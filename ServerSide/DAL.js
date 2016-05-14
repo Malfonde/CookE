20398a12e39852c5d6589416e17a4ad4d1f73aed
lielc <zaboolina@gmail.com>
2016-05-14 22:37:44 +0300

    Added ServerSide folder

---
+++ ServerSide/DAL.js
@@ -0,0 +1,116 @@
+﻿var mydb = require('./myDB')
+ , ObjectID = require('mongodb').ObjectID;
+
+var connection;
+mydb.openDatabase(function (err, db) {
+    if (db != null) {
+        connection = db;
+    }
+});
+
+module.exports = {
+
+    getAllRecipes: function (callback) {
+        // Return all the Recipes in the db
+        var results;
+        connection.collection("Recipes").find({}).toArray(function (err, messages) {
+            if (err) {
+                console.log('err:' + err);
+                callback(err, results);
+            }
+            else {
+                results = messages;
+                callback(null, results);
+            }
+        });
+    },
+
+    saveNewRecipe: function (newRecipe, callback) {
+        var collection = connection.collection("Recipes");
+
+        // Insert new Recipe to Recipes collection
+        var result = collection.insert({
+            Name: newRecipe.name,
+			UserID: newRecipe.userID,
+            CookingInstructions: newRecipe.cookingInstructions,
+			ServingInstructions: newRecipe.servingInstructions,
+			Description: newRecipe.description,
+			Ingredients: newRecipe.ingredients,
+			ImagePath: newRecipe.image
+			
+        }, function (err, data) {
+            if (err) {
+                console.log('new recipe error: ' + err);
+                callback(err, data);
+            }
+            else {
+				console.log('new recipe success');
+                callback(null, data.ops[0]);
+            }
+        });
+    },
+	
+	addNewUser: function (newUser, callback){
+		var collection = connection.collection("Users");
+		
+		var result = collection.insert({
+			Favorites: newUser.favorites			
+		}, function (err, data){
+			
+			if(err){
+				console.log('new user error: ' + err);
+				callback(err,data);
+			}
+			else{
+				console.log('new user success');
+                callback(null, data.ops[0]);
+			}			
+		});
+	},
+	
+	
+	getAllRecipesByUser: function(userId,callback){
+		// Returns all the recipes of the user
+		var results;
+		connection.collection("Recipes").find({UserID:userId}).toArray(function (err, recipes) {
+            if (err) {
+                console.log('err:' + err);
+                callback(err, recipes);
+            }
+            else {
+                results = recipes;				
+                callback(null, results);
+            }
+        });		
+	},
+	
+	// returns recipes by query
+	getRecipesByQueryArray: function (recipesQuery, callback){
+		
+		var results;
+		connection.collection("Recipes").find({ $or: recipesQuery }).toArray(function (err, retRecipes) 
+		{
+			if(err)
+			{
+				console.log(err);		
+				callback(err, results);
+			}
+			else
+			{
+				results = retRecipes;
+				console.log(results);				
+				callback(null, results);
+			}			
+		});
+	},
+	
+	addRecipeToUserFavorits: function (recipeToAdd, userID, callback){		
+		var collection = connection.collection("Users");
+		
+		var updatedUser = {
+                Favorites:  
+            };
+		
+		
+	}
+};
---
+++ ServerSide/Server.js
@@ -0,0 +1,72 @@
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , bodyParser = require('body-parser');
+
+server.listen(8080);
+var bl = require('./BL');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.post('/saveRecipe',function(req,res)
+{ 	
+	bl.addRecipe(req ,res);
+}); 
+
+app.post('/addUser',function(req,res)
+{ 	
+	bl.addUser(req ,res);
+}); 
+
+var os = require('os');
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+
+console.log(addresses);
+
+
+
+/*app.get('/saveRecipe', function (request, response) {
+    console.log("saveRecipe: " + request.params.id);
+
+    var wantedScreenId = Number(request.params.id);
+
+    // iterate over the ads received from the server and return only the relevent ones
+    var availableAds = getAds().filter(function (currAd) {
+        return currAd.isAdSuitable(wantedScreenId);
+    });
+
+    // If no ad can be displayed
+    if (availableAds.length == 0) {
+        response.sendfile("./noAds.html");
+    }
+    else {
+        // Return a random ad that fits the criteria
+        response.json(availableAds[Math.floor((Math.random() * availableAds.length))]);
+    }
+});*/
+
+//--- Apriori Algorithm --- 
+app.post('/Server/getRecommandedRecipesByUser', function (req, res) 
+{  
+	bl.getRecommandedRecipesByUser(req, res);    
+});
+
+app.post('/Server/addRecipeToUserFavorits', function (req, res) 
+{  
+	bl.addRecipeToUserFavorits(req, res);    
+});
+
